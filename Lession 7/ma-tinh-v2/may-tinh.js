8fdcfa189e510d8c75fd2bec4639e6d30dd34621
nguyenhieu195 <nguyenhieu1952005@gmail.com>
2025-08-17 19:37:00 +0700

    up

--- Lession 7/ma-tinh-v2/may-tinh.js
+++ Lession 7/ma-tinh-v2/may-tinh.js
@@ -1,4 +1,29 @@
 function number(nums){
-    let result = document.getElementById("nums").value;
-    
+    document.getElementById("result").value += nums;
+}
+function phimBang(){
+    let Result = eval(document.getElementById("result").value);
+    document.getElementById("result").value = Result;
+}
+function xoa(){
+    document.getElementById('result').value = '';
+}
+function phepCong(){
+    document.getElementById("result").value += " + ";
+}
+function phepTru(){
+    document.getElementById('result').value += " - ";
+}
+function phepNhan(){
+    document.getElementById('result').value += " * ";
+}
+function phepChia(){
+    document.getElementById('result').value += ' / ';
+}
+function phanTram(){
+    let phantram = (eval(document.getElementById('result').value));
+    document.getElementById('result').value = phantram / 100;
+}
+function giaiThua(){
+    document.getElementById('result').value += "!";
 }
