f1f2244e4bc84f063f69ad10332327c51558aaee
nguyenhieu195 <nguyenhieu1952005@gmail.com>
2025-10-08 20:55:32 +0700

    up

---
+++ Module2/Project/src/pj2/hienThi20SNTdauTien.java
@@ -0,0 +1,12 @@
+package pj2;
+
+public class hienThi20SNTdauTien {
+    public static boolean snt(int ) {
+        if(i < )
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+
+        }
+    }
+}
