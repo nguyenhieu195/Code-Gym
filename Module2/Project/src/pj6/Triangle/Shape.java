131f861fad52a01036fd8700546ea1e9cd95617a
nguyenhieu195 <nguyenhieu1952005@gmail.com>
2025-10-17 21:19:10 +0700

    up

---
+++ Module2/Project/src/pj6/Triangle/Shape.java
@@ -0,0 +1,19 @@
+package pj6.Triangle;
+
+public class Shape {
+    private String color;
+
+    public Shape() {}
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
